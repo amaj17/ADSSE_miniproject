8a651911ad6c02b4309f91bc8076ee019a962b2b
amaj17 <36537109+amaj17@users.noreply.github.com>
2021-04-29 12:35:29 +0200

    Update brute-force.py

--- brute-force.py
+++ brute-force.py
@@ -17,19 +17,29 @@ def brute_force(a, n):
                 x2 = j
     return x1, x2, maximum
 
+#Generate an array (here size of 50000)
 a = np.random.randint(-10, 10, 50000)
 a = np.array(a)
+
+#Start tracking the memory
 tracemalloc.start()
+#Start tracking the time
 begin = time.time()
 
 max_sum_subarray_brute = brute_force(a, len(a))
+
+#End tracking time
 end = time.time()
+#End tracking memory
 snapshot= tracemalloc.take_snapshot()
 
+#Print collected information
 for stat in snapshot.statistics("lineno"):
     print("stat brute")
     print(stat)
     print(stat.traceback.format())
+#Print Peak memory
 print("\nTraced Memory (Current, Peak): ", tracemalloc.get_traced_memory())
+#Print time spent
 print("Time:")
 print(end-begin)
